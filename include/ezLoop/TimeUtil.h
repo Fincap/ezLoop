56890ddc33e3489b9c813a79c086e145df50da63
fincap <finnkerman@gmail.com>
2021-06-23 00:26:30 +1000

    Moved implementations into header files, header-only now.

--- include/ezLoop/TimeUtil.h
+++ include/ezLoop/TimeUtil.h
@@ -4,6 +4,14 @@
 
 namespace ez {
 
-	double getCurrentTime();
+	// Returns the current epoch time in milliseconds as a double.
+	double getCurrentTime()
+	{
+		return static_cast<double>(
+			std::chrono::duration_cast<std::chrono::milliseconds>(
+				std::chrono::system_clock::now().time_since_epoch()
+				).count()
+			);
+	}
 
 }
